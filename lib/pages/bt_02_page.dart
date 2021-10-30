5064091339236d4cb832f70d907d2e1e52029780
14h4idev <pgkhai.dev@gmail.com>
2021-10-30 22:00:16 +0700

    bt2

--- lib/pages/bt_02_page.dart
+++ lib/pages/bt_02_page.dart
@@ -1,15 +1,53 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class Bt02Page extends StatelessWidget {
+class Bt02Page extends StatefulWidget {
   const Bt02Page({Key? key}) : super(key: key);
 
+  @override
+  State<Bt02Page> createState() => _Bt02PageState();
+}
+
+class _Bt02PageState extends State<Bt02Page> {
+  Response? response;
+  bool isLoading = false;
+  String error = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BT 02'),
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.cloud_download,
+        ),
+        onPressed: _getData,
+      ),
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : response == null
+                ? const Text('Empty data')
+                : response!.statusCode == 200
+                    ? Text('${response!.data}')
+                    : Text(error),
+      ),
     );
+  }
+
+  _getData() async {
+    String url = 'https://api.dofhunt.200lab.io/v1/categoriess';
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      response = await Dio().get(url);
+    } catch (e) {
+      error = 'Has error: $e';
+    }
+    setState(() {
+      isLoading = false;
+    });
   }
 }
